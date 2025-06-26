495a2f22136f8b6847f1be333b40bce5146f583c
TheMrDuck <amerswedan41@gmail.com>
2025-06-26 14:24:21 +0300

    norm to go

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aswedan <aswedan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 15:49:59 by aswedan           #+#    #+#             */
-/*   Updated: 2025/06/10 15:50:02 by aswedan          ###   ########.fr       */
+/*   Updated: 2025/06/26 14:23:33 by aswedan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,39 +18,45 @@ void	exit_error(char *msg)
 	exit(1);
 }
 
-int main(int ac, char **av)
+void	joining_threads(t_philo *philos, t_info *philos_info)
 {
-	t_philo philos[PHILO_MAX];
-	pthread_mutex_t forks[PHILO_MAX];
-	t_info *philos_info;
-	int i;
+	int	i;
 
-	if (ac < 5 || ac > 6)
-		exit_error("Wrong number of arguments\n");
-	
-	philos_info = malloc(sizeof(t_info));
-	if (!philos_info)
-		exit_error("Memory allocation failed\n");
-	
-	initializer(av, philos, forks, philos_info);
-	create_threads(philos, philos_info);
-	
 	i = 0;
 	while (i < philos_info->num_of_philos)
 	{
 		pthread_join(philos[i].thread, NULL);
 		i++;
 	}
-	
+}
+
+void	forks_destroyer(t_info *philos_info, pthread_mutex_t *forks)
+{
+	int	i;
+
 	i = 0;
 	while (i < philos_info->num_of_philos)
 	{
 		pthread_mutex_destroy(&forks[i]);
 		i++;
 	}
+}
+
+int	main(int ac, char **av)
+{
+	t_philo			philos[PHILO_MAX];
+	pthread_mutex_t	forks[PHILO_MAX];
+	t_info			*philos_info;
+
+	if (ac < 5 || ac > 6)
+		exit_error("Wrong number of arguments.\nUsage: [Num of philos] [Time to die] [Time to eat] [Time to sleep] Optional: [Num of meals]\n");
+	philos_info = malloc(sizeof(t_info));
+	if (!philos_info)
+		exit_error("Memory allocation failed\n");
+	initializer(av, philos, forks, philos_info);
+	create_threads(philos, philos_info);
+	joining_threads(philos, philos_info);
 	pthread_mutex_destroy(&philos_info->timing_mutex);
-	
 	free(philos_info);
 	return (0);
 }
-
--- philos_routine.c
+++ philos_routine.c
@@ -6,7 +6,7 @@
 /*   By: aswedan <aswedan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 15:50:11 by aswedan           #+#    #+#             */
-/*   Updated: 2025/06/10 16:16:35 by aswedan          ###   ########.fr       */
+/*   Updated: 2025/06/26 14:24:14 by aswedan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void is_eating(t_philo *philo)
 {
     size_t current_time;
-    
+
     if (philo->id % 2 == 0)
     {
         pthread_mutex_lock(philo->r_fork);
@@ -40,23 +40,23 @@ void is_eating(t_philo *philo)
     pthread_mutex_unlock(philo->r_fork);
 }
 
-void is_thinking(t_philo *philo)
+void is_sleeping(t_philo *philo)
 {
     size_t current_time;
-    
+
     pthread_mutex_lock(&philo->info->timing_mutex);
     current_time = get_timestamp();
     if (philo->info->simulation_flag)
         printf("%zu %d is sleeping\n", (current_time - philo->info->timestamp), philo->id);
     pthread_mutex_unlock(&philo->info->timing_mutex);
-    
+
     ft_usleep(philo->info->time_to_sleep);
 }
 
 void is_thinking(t_philo *philo)
 {
     size_t current_time;
-    
+
     pthread_mutex_lock(&philo->info->timing_mutex);
     current_time = get_timestamp();
     if (philo->info->simulation_flag)
@@ -67,19 +67,19 @@ void is_thinking(t_philo *philo)
 void	*routine(void *arg)
 {
     t_philo *philo = (t_philo *)arg;
-    
+
     if (philo->id % 2 == 0)
         ft_usleep(philo->info->time_to_eat / 2);
-    
+
     while (philo->info->simulation_flag && philo->is_alive)
     {
         is_eating(philo);
-        
-        if (philo->info->num_of_meals != -1 && 
+
+        if (philo->info->num_of_meals != -1 &&
             philo->num_of_meals_eaten >= philo->info->num_of_meals)
             break;
-            
-        is_thinking(philo);
+
+        is_sleeping(philo);
         is_thinking(philo);
     }
     return (NULL);
